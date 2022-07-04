6e64e2f3bbcfb546731fa38f2187434089addfb6
pabloacalvo <103800030+pabloacalvo@users.noreply.github.com>
2022-07-03 23:39:52 -0300

    creacionUsuarios

--- DataStore/DatosMaestros.cs
+++ DataStore/DatosMaestros.cs
@@ -60,6 +60,13 @@ namespace DataStore
             LstCountries = dbOperation.OperationQuery<Country>(sql);
             return LstCountries;
         }
+        
+       public List<Person>GetPersons()
+        {
+            List <Person> LstGetPersons = new List<Person>();
+            string sql=
+            return LstPerson;
+        }
 
 
     }
--- Services/SecurityServices.cs
+++ Services/SecurityServices.cs
@@ -37,6 +37,20 @@ namespace Services
             return datosMaestros.GetCountries();
         }
 
+        public User login(String txtUser, String txtPass)
+        {
+            User result = null;
+            List<User> users = this.getUsers();
+            foreach (User user in users)
+            {
+                if (user.username == txtUser && user.pass == txtPass)
+                {
+                    result = user;
+                    break;
+                }
+            }
+            return result;
+        }
 
 
 
